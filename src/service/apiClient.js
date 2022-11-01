1e0f62a7f224c3e8c10bb86c6b63f57aed923a35
atomfaster@gmail.com <atomfaster@gmail.com>
2022-11-01 13:37:41 +0700

    deploy

--- src/service/apiClient.js
+++ src/service/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://18.234.126.238:8999",
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',

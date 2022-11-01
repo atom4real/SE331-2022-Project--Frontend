0e5cf83c6274aad505624784b79a5b79cc88af5d
atomfaster@gmail.com <atomfaster@gmail.com>
2022-11-01 15:01:47 +0700

    fix

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -14,7 +14,6 @@
           </label>
           <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
             <li @click="profile"><a>Profile</a></li>
-            <li><a>Update</a></li>
             <li @click="logout"><a>Logout</a></li>
           </ul>
         </div>

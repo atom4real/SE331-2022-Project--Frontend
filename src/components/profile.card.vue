1ab5176d50ef16a76a4c4bd3fca17cee1a8aed1f
atomfaster@gmail.com <atomfaster@gmail.com>
2022-11-01 15:00:34 +0700

    fix

--- src/components/profile.card.vue
+++ src/components/profile.card.vue
@@ -67,7 +67,7 @@ const props = defineProps({
             "homeTown": "Chiang mai",
             "email": "wanatchaya.idea@gmail.com",
             "birthDate": "06/01/45 12:03",
-            "imageUrls": "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            "imageUrls": "https://images.unsplash.com/photo-1582750433449-648ed127bb54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
 
         }
     }
